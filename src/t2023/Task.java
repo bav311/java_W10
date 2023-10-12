4731a2bdec5ae0ce64d93dfdab7c65d243610c26
bav311-W10 <bav311@yandex.ru>
2023-10-12 18:34:55 +0500

    second commit: add new task

--- src/t2023/Task.java
+++ src/t2023/Task.java
@@ -1,8 +1,14 @@
-package t2023;
+/*Реализуй метод getCountSecondsPassedInCurrentHour(int seconds).
+Метод на вход принимает целое число секунд, которые прошли с начала текущих суток.
+Нужно найти количество секунд, прошедших с начала последнего часа, и вернуть это число.
+Пример: Метод getCountSecondsPassedInCurrentHour вызывается с параметром 12345.
+Пример вывода: 1545*/
+
+		package t2023;
 
 public class Task {
 	public static void main(String[] args) {
-		
+
 
 	}
 }
