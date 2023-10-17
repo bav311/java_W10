178ac3f745debcff303bb31af6b354fbdcabfa8e
bav311-W10 <bav311@yandex.ru>
2023-10-17 21:58:19 +0500

    ConcatArray

--- src/t2023/Task.java
+++ src/t2023/Task.java
@@ -1,27 +1,7 @@
 package t2023;
 
-import java.util.Arrays;
-
 public class Task {
 	public static void main(String[] args) {
-		int[] firstArray = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-		int[] secondArray = new int[]{10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
-		int[] resultArray = new int[firstArray.length + secondArray.length];
-
-		for (int i = 0; i < firstArray.length; i++) {
-			resultArray[i] = firstArray[i];
-		}
-
-		for (int i = 0; i < secondArray.length; i++) {
-			resultArray[firstArray.length + i] = secondArray[i];
-		}
-
-
-
-
-		System.out.println(Arrays.toString(resultArray));
-		System.out.println(resultArray.length);
-
 
 	}
 }
