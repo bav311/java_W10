bcd2f03d15e46ef87a8446a6d21bcfdb26323ee9
bav311-W10 <bav311@yandex.ru>
2023-10-17 21:58:19 +0500

    ConcatArray

--- src/t2023/ConcatArray.java
+++ src/t2023/ConcatArray.java
@@ -2,7 +2,7 @@ package t2023;
 
 import java.util.Arrays;
 
-public class Task {
+public class ConcatArray {
 	public static void main(String[] args) {
 		int[] firstArray = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 		int[] secondArray = new int[]{10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
@@ -16,12 +16,7 @@ public class Task {
 			resultArray[firstArray.length + i] = secondArray[i];
 		}
 
-
-
-
 		System.out.println(Arrays.toString(resultArray));
 		System.out.println(resultArray.length);
-
-
 	}
 }
--- src/t2023/SumNumber.java
+++ src/t2023/SumNumber.java
@@ -2,16 +2,13 @@ package t2023;
 
 public class SumNumber {
 	public static void main(String[] args) {
-
 		System.out.println(getSum(546));
 	}
-
 	public static int getSum(int num) {
 		int sum = 0;
 		while (num > 0) {
 			sum = sum + num % 10;
 			num /= 10;
-
 		}
 		return sum;
 	}
