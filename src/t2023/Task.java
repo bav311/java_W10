7c55350292cc374d7abf761a457c35fe3f8a4139
bav311-W10 <bav311@yandex.ru>
2023-10-23 20:17:33 +0500

    Fill array

--- src/t2023/Task.java
+++ src/t2023/Task.java
@@ -1,9 +1,22 @@
+//https://javarush.com/quests/lectures/questsyntaxpro.level05.lecture07
 package t2023;
+/*
+Реализуй статический метод main(String[]), который заполняет массив array значениями valueStart и valueEnd.
+Если длина массива четная, его первую половину нужно заполнить значениями valueStart,
+а вторую — valueEnd. Если длина массива нечётная, то первую большую часть заполнить значениями valueStart,
+вторую меньшую — valueEnd.
+Для заполнения массива используй метод Arrays.fill(int[], int, int, int).
+При тестировании значения полей класса Solution будут разными, учти это.
+*/
 
 public class Task {
+	public static int[] array = new int[20];
+	public static int valueStart = 10;
+	public static int valueEnd = 13;
+
 	public static void main(String[] args) {
-		
 
 
 	}
 }
+
