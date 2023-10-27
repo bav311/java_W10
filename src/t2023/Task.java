5a2f377efadc2a6cbfe4580eb65664779686ed2f
Andrew from Job <bav311@yandex.ru>
2023-10-27 15:29:04 +0500

    fill array

--- src/t2023/Task.java
+++ src/t2023/Task.java
@@ -2,7 +2,8 @@
 //https://javarush.com/groups/posts/mnogomernye-massivy#%D0%94%D0%B2%D1%83%D0%BC%D0%B5%D1%80%D0%BD%D1%8B%D0%B5-%D0%BC%D0%B0%D1%81%D1%81%D0%B8%D0%B2%D1%8B
 package t2023;
 /*
-Реализуй статический метод main(String[]), который заполняет массив array значениями valueStart и valueEnd.
+Реализуй статический метод main(String[]), который заполняет массив array значениями
+valueStart и valueEnd.
 Если длина массива четная, его первую половину нужно заполнить значениями valueStart,
 а вторую — valueEnd. Если длина массива нечётная, то первую большую часть заполнить значениями valueStart,
 вторую меньшую — valueEnd.
@@ -10,13 +11,16 @@ package t2023;
 При тестировании значения полей класса Solution будут разными, учти это.
 */
 
+import java.util.Arrays;
+
 public class Task {
-	public static int[] array = new int[20];
-	public static int valueStart = 10;
-	public static int valueEnd = 13;
+    public static int[] array = new int[21];
+    public static int valueStart = 10;
+    public static int valueEnd = 13;
 
-	public static void main(String[] args) {
-
-
-	}
+    public static void main(String[] args) {
+        Arrays.fill(array, 0, array.length / 2 + (array.length % 2), valueStart);
+        Arrays.fill(array, array.length / 2 +(array.length % 2), array.length, valueEnd);
+        System.out.println(Arrays.toString(array));
+    }
 }
