de32d1a2926d5711eae76fa0239cfcfbbe260424
Andrew from Job <bav311@yandex.ru>
2023-11-24 11:11:09 +0500

    BinarySearch

---
+++ src/t2023/BinarySearch.java
@@ -0,0 +1,48 @@
+//https://javarush.com/quests/lectures/questsyntaxpro.level06.lecture05
+// помощь https://javarush.com/help
+//задачи 7 уровня https://javarush.com/tasks?quest=QUEST_JAVA_SYNTAX&level=7&page=2
+
+package t2023;
+
+public class BinarySearch {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 8, 35, 40, 41, 44, 56, 65, 94, 99, 100, 101};
+
+        printArr(arr);
+        System.out.println();
+        System.out.println(binarySearch(arr,41));
+
+
+
+    }
+
+        public static int binarySearch(int[] arr, int target) {
+            int left = 0;
+            int right = arr.length - 1;
+
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+
+                if (arr[mid] == target) {
+                    return mid; // Найден, возвращаем индекс
+                } else if (arr[mid] < target) {
+                    left = mid + 1; // Целевой элемент находится в правой половине
+                } else {
+                    right = mid - 1; // Целевой элемент находится в левой половине
+                }
+            }
+
+            return -1; // Элемент не найден
+        }
+
+
+
+
+
+    private static void printArr(int[] arr) {
+        for (int a : arr) {
+            System.out.print(a + " ");
+        }
+    }
+}
