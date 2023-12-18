73c7102f29cb10660abe688b1d30860ed0866406
bav311-W10 <bav311@yandex.ru>
2023-12-18 20:50:28 +0500

    leetcode Arrays 101

--- src/t2023/Task.java
+++ src/t2023/Task.java
@@ -5,32 +5,28 @@
 package t2023;
 
 public class Task {
-    public static void main(String[] args) {
-        String a = "dermatoglyphics"; //true
-        String a1 =  "moose"; // false
-        String a2 =  "aba"; // false
-        System.out.println(a.charAt(2));
+	public static void main(String[] args) {
+		String a = "dermatoglyphics"; //true
+		String a1 = "moose"; // false
+		String a2 = "abba"; // false
+		System.out.println(isIsogram(a));
+		System.out.println(isIsogram(a1));
+		System.out.println(isIsogram(a2));
+
+	}
+
+	public static boolean isIsogram(String str) {
+		char[] charArray = str.toCharArray();
+		int count = 0;
+		for (int i = 0; i < charArray.length - 1; i++) {
+			for (int j = i + 1; j < charArray.length; j++) {
+				if (charArray[i] == charArray[j]) {
+					count++;
+				}
+			}
+		}
+		return count != 0;
+	}
 
 
-
-
-
-
-    }
-    public static boolean  isIsogram(String str) {
-        char [] test = str.toCharArray();
-        int count = 0;
-        for (int i = 0; i < test.length; i++) {
-            char tmp = test[i];
-            for (int j = 1; j < test.length -1; j++) {
-
-            }
-
-        }
-
-
-
-        return false;
-    }
-    }
-
+}
--- src/t2023/Task2.java
+++ src/t2023/Task2.java
@@ -1,10 +1,23 @@
 package t2023;
 
+import java.util.Arrays;
+
 public class Task2 {
 
 	public static void main(String[] args) {
-
+		int x = 92;
+		System.out.println(Arrays.toString(reverse(x)));
 
 
 	}
+
+	public static int[] reverse(int n) {
+		int[] arr = new int[n];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = n -i;
+		}
+		return arr;
+	}
+
+
 }
